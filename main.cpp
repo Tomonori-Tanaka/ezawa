83e1da97cf155c9caf635a3f78bf1537b7caf35d
tomorin <tanaka.t.bj@gmail.com>
2021-11-08 16:55:19 +0900

    27.2 p323 2

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ struct Object {
     void member_function() {
         std::cout << data_member << std::endl;
     }
+
+    void f1(int) {
+        std::cout << 1 << std::endl;
+    }
+
+    void f2(int, int) {
+        std::cout << 2 << std::endl;
+    }
 };
 
 int main() {
@@ -22,9 +30,14 @@ int main() {
 
     Object *object_ptr = &object;
     (*object_ptr).data_member = 7;
+    object_ptr->*data_ptr = 9;
     (*object_ptr).member_function();
     std::invoke(data_ptr, object_ptr) = 8;
     (*object_ptr).member_function();
+
+    (object_ptr->*&Object::f1)(1);
+    std::invoke(&Object::f1, object_ptr, 1);
+    std::invoke(&Object::f2, object_ptr, 3, 3);
 
 
     return 0;
