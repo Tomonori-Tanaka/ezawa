1ec9051c04569d283ebeb6c1df002a3d191cef4c
tomorin <tanaka.t.bj@gmail.com>
2021-12-20 20:16:33 +0900

    29.6

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
 #include <limits>
 #include <array>
 
+template<class T>
+void print(T var) {
+    std::cout << var << std::endl;
+}
 
+class Logger {
+public:
+    std::string name;
 
+    Logger(std::string name)
+            : name(name) {
+        std::cout << name << " is constructed\n";
+    }
+
+    ~Logger() {
+        std::cout << name << " is destructed\n";
+    }
+};
 
 int main() {
 
-    void * ptr = malloc(5);
-    std::free(ptr);
+    void *ptr = ::operator new(sizeof(Logger));
+    Logger *logger_ptr = new(ptr) Logger{"Alice"};
+    logger_ptr->~Logger();
+    ::operator delete(ptr);
 
-    void * ptr2 = ::operator new(10);
-    ::operator delete(ptr2);
 
     return 0;
 }
