5effc632b98986073d036751f2834b76ceb53756
tomorin <tanaka.t.bj@gmail.com>
2021-12-20 20:32:54 +0900

    29.8

--- main.cpp
+++ main.cpp
@@ -22,10 +22,23 @@ public:
 
 int main() {
 
-    void *ptr = ::operator new(sizeof(Logger));
-    Logger *logger_ptr = new(ptr) Logger{"Alice"};
-    logger_ptr->~Logger();
-    ::operator delete(ptr);
+    int * int_ptr = new int{};
+    print(*int_ptr);
+    std::vector<double> * vec_double = new std::vector<double>{{12, 13}};
+    print(vec_double->at(1));
+
+    delete int_ptr;
+    delete vec_double;
+
+    try {
+        new int{100000000};
+    } catch (std::bad_alloc e) {
+        print(e.what());
+    }
+
+    int * int_array_ptr = new int[4]{1, 2, 3, 4};
+    print(int_array_ptr[3]);
+    delete[] int_array_ptr;
 
 
     return 0;
